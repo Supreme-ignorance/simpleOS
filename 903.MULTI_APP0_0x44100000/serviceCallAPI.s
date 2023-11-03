4575e2ed6750700af17b95e85a32ba4ffd22313d
gyoungna <leelyduho79@gmail.com>
2023-11-03 10:13:08 +0900

    add svc to app0,app1

---
+++ 903.MULTI_APP0_0x44100000/serviceCallAPI.s
@@ -0,0 +1,67 @@
+	.include "4412addr.inc"
+	.include "option.inc"
+
+	.code	32
+	.text
+
+
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@
+@svc call api
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@
+@Lcd_Clr_Screen 10
+@Lcd_Draw_BMP 11
+@Uart_Printf 21
+@LED_Display 12
+@Key_Get_Key_Pressed 30
+@Key_Wait_Key_Released 31
+@Key_Wait_Key_Pressed 32
+@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
+
+
+	.global Lcd_Clr_Screen
+Lcd_Clr_Screen:
+
+	svc 10
+	bx lr
+
+
+	.global Lcd_Draw_BMP
+Lcd_Draw_BMP:
+
+	svc 11
+	bx lr
+
+	.global Uart1_Printf
+Uart1_Printf:
+
+	svc 21
+	bx lr
+
+	.global LED_Display
+LED_Display:
+
+	svc 12
+	bx lr
+
+
+	.global Key_Get_Key_Pressed
+Key_Get_Key_Pressed:
+
+	svc 30
+	bx lr
+
+	.global Key_Wait_Key_Released
+Key_Wait_Key_Released:
+
+	svc 31
+	bx lr
+
+	.global Key_Wait_Key_Pressed
+Key_Wait_Key_Pressed:
+
+	svc 32
+	bx lr
