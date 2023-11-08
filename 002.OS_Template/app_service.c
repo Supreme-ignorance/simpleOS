53e8b333530b1f10897f327727edcbdbd150a494
unknown <wishing12@kyonggi.ac.kr>
2023-11-08 23:18:18 +0900

    modify section

--- 002.OS_Template/app_service.c
+++ 002.OS_Template/app_service.c
@@ -14,8 +14,6 @@ unsigned int ram[] = {RAM_APP0, RAM_APP1};
 unsigned int sizeApp[] = {SIZE_APP0, SIZE_APP1};
 unsigned int stackLimit[] = {STACK_LIMIT_APP0, STACK_LIMIT_APP1};
 unsigned int stackBase[] = {STACK_BASE_APP0, STACK_BASE_APP1};
-unsigned int pcb[] = {PCBTABLE_BASE_APP0, PCBTABLE_BASE_APP1};
-unsigned int sizePcb[] = {SIZE_PCB0, SIZE_PCB1};
 
 int initApp(int appNum)
 {
@@ -24,16 +22,15 @@ int initApp(int appNum)
 	int i;
 	for (i = 0; i < 2; i++){
 		Init_App(ram[0], stackBase[0], stackBase[1]);
+		// 스택 (LOCAL)
 		SetAppTransTable(stackLimit[i], stackBase[i]-1, stackLimit[i], RW_WBWA_LOCAL, i);
-		// 2차 테이블
+		// 2차 테이블 (LOCAL)
 		SetAppTransTablePageTable(ram[0], (ram[0]+sizeApp[0]-1), PAGE_1ST_RW, PAGE_2ST_RW_NCNB_LOCAL_NO_ACCESS, i);
-		// pcb
-		SetAppTransTable(pcb[i], pcb[i]+sizePcb[i]-1, ram[i], RW_WBWA, i);
-		// mataDemandPage
-		SetAppTransTable(META_DEMAND_PAGE, META_DEMAND_PAGE + SIZE_META - 1, META_DEMAND_PAGE, RW_WBWA, i);
-		// DemandPage
+		// mataDemandPage (GLOBAL)
+		SetAppTransTable(META_TABLE_BASE_APP, META_TABLE_LIMIT_APP - 1, META_TABLE_BASE_APP, RW_WBWA, i);
+		// DemandPage (GLOBAL)
 		SetAppTransTable(DEMAND_PAGE_BASE, DEMAND_PAGE_BASE + SIZE_DEMAND_PAGE-1, DEMAND_PAGE_BASE, RW_WT, i);
-		// 프로그램 메모리
+		// 프로그램 메모리 (LOCAL)
 		SetAppTransTable(0x84100000, 0x84500000-1, ram[i], RW_WBWA_LOCAL, i);
 	}
 
